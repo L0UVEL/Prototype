207ce98a323f7027e2fffb67e1f57df9e88704b5
L0UVEL <louvelrouz13@gmail.com>
2026-05-04 19:14:12 +0800

    Finding errors at dagdagan ito

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ls -la
 
 # 1. Download the Flutter SDK
 if [ ! -d "flutter" ]; then
@@ -9,8 +10,11 @@ fi
 # 2. Add Flutter to the Path
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# 3. Enable Web support and Build
+# 3. Enable Web support and Install dependencies
 flutter config --enable-web
+flutter pub get
+
+# 4. Build the Web App
 flutter build web --release --web-renderer html
 
 # 4. Clean up (Optional, helps keep the build size smaller)
